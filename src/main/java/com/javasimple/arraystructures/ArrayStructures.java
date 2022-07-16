8b290e4657125ea82fb617c3c11219f8a79449bb
mert18 <mertuygur02@gmail.com>
2022-07-16 23:39:32 +0300

    bubble sort added

--- src/main/java/com/javasimple/arraystructures/ArrayStructures.java
+++ src/main/java/com/javasimple/arraystructures/ArrayStructures.java
@@ -1,4 +1,4 @@
-package com.thealgorithms.arraystructures;
+package com.javasimple.arraystructures;
 
 public class ArrayStructures {
     private int[] theArray = new int[50];
@@ -49,20 +49,67 @@ public class ArrayStructures {
         theArray[arraySize-1] = value;
     }
 
+    public void bubbleSort(){
+        for(int i = arraySize - 1; i > 1; i--){
+            for(int j=0; j<i; j++){
+                if(theArray[j] > theArray[j+1]){
+                    swapValues(j, j+1);
+                }
+            }
+        }
+    }
+
+    public void binarySearch(int value){
+        int lowIndex = 0;;
+        int highIndex = arraySize -1;
+        while(lowIndex <= highIndex){
+            int middleIndex = (highIndex + lowIndex) / 2;
+            if(theArray[middleIndex] < value){
+                lowIndex = middleIndex +1;
+
+            } else if(theArray[middleIndex] > value)
+            {
+                highIndex = middleIndex - 1;
+            }
+            else{
+                System.out.println("\n Found!, It is at index " + middleIndex);
+                lowIndex = highIndex + 1;
+            }
+        }
+    }
+
+    public void swapValues(int indexOne, int indexTwo){
+        int temp = theArray[indexOne];
+        theArray[indexOne] = theArray[indexTwo];
+        theArray[indexTwo] = temp;
+    }
+
     public static void main(String[] args){
         ArrayStructures newArray = new ArrayStructures();
 
         newArray.generateRandomArray();
-
+        System.out.println("Randomly generated array!");
         newArray.printArray();
 
+        System.out.println("What is the at the index 3?");
         System.out.println(newArray.getValueAtIndex(3));
+
+        System.out.println("Is 13 present?");
         System.out.println(newArray.isPresent(13));
+
+        System.out.println("Deleting index 2");
         newArray.deleteIndex(2);
         newArray.printArray();
 
+        System.out.println("Inserting value 333!");
         newArray.insertValue(333);
         newArray.printArray();
+
+        System.out.println("After Bubble Sort!");
+        newArray.bubbleSort();
+
+        System.out.println("Binary Search for 333!");
+        newArray.binarySearch(333);
 
     }
 }
